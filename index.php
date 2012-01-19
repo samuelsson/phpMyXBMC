41523a272770329c49b1fd820be1dd4cd3cd3754
Erik Samuelsson <durkslag@gmail.com>
2012-01-19 23:48:37 +0100

    File and Code clean up

--- index.php
+++ index.php
@@ -1,76 +1,8 @@
 <?php
-	
-function _get_hash($file_path)
-  {
-    $chars = strtolower($file_path);
-    $crc = 0xffffffff;
+	require_once('inc/functions.php');
+	get_header();
+?>
 
-    for ($ptr = 0; $ptr < strlen($chars); $ptr++)
-    {
-      $chr = ord($chars[$ptr]);
-      $crc ^= $chr << 24;
-
-      for ((int) $i = 0; $i < 8; $i++)
-      {
-        if ($crc & 0x80000000)
-        {
-          $crc = ($crc << 1) ^ 0x04C11DB7;
-        }
-        else
-        {
-          $crc <<= 1;
-        }
-      }
-    }
-
-    // Is system 64 bits ?
-    if (strpos(php_uname('m'), '_64') !== false)
-    {
-
-			//Formatting the output in a 8 character hex
-			if ($crc>=0)
-			{
-				$hash = sprintf("%16s",sprintf("%x",sprintf("%u",$crc)));
-			}
-			else
-			{
-				$source = sprintf('%b', $crc);
-
-				$hash = "";
-				while ($source <> "")
-				{
-					$digit = substr($source, -4);
-					$hash = dechex(bindec($digit)) . $hash;
-					$source = substr($source, 0, -4);
-				}
-			}
-			$hash = substr($hash, 8);
-    }
-    else
-    {
-			//Formatting the output in a 8 character hex
-			if ($crc>=0)
-			{
-				$hash = sprintf("%08s",sprintf("%x",sprintf("%u",$crc)));
-			}
-			else
-			{
-				$source = sprintf('%b', $crc);
-
-				$hash = "";
-				while ($source <> "")
-				{
-					$digit = substr($source, -4);
-					$hash = dechex(bindec($digit)) . $hash;
-					$source = substr($source, 0, -4);
-				}
-			}
-    }
-
-    return $hash;
-  }
-
-$temp = _get_hash("/media/Media/Filmer/HD/The.Mummy.Returns.2001.720p.BluRay.DTS.x264-RuDE/");
-echo $temp;
-
+<?php
+	get_footer();
 ?>
